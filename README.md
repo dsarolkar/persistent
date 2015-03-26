072f3b9924b68f747308780bb1295276eff230a3
pjoshi-quixey <pjoshi-consultant@quixey.com>
2015-03-26 13:31:51 -0700

    Commit from master

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ## persistent
 ## More changes in Master - master has moved ahead
+This is a change in master - we need to ensure that we see this line when doing a rebase
--- common.java
+++ common.java
@@ -1,3 +1,4 @@
 First Edit by Devdatta
 
 ## More changes in Master - master has moved ahead
+This is a change in master - we need to ensure that we see this line when doing a rebase
